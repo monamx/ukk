202d384d7bcc4dd97cc7f185dde2a707ee44b77a
monamx <129038957+monamx@users.noreply.github.com>
2024-02-28 01:29:41 +0700

    dash page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+import Navbar from "../components/Navbar/page"
+import Sidebar from "../components/Sidebar/page"
+// import Data from "../../Component/Data/page"
+// import Form from "../../Component/Form/page"
+import { useState } from 'react';
+import { ChevronsUp, ChevronsDown } from 'react-feather';
+
+export default function DataBarang() {
+  const [isSidebarCollapsed, setSidebarCollapsed] = useState(true);
+  const [isSHForm, setSHForm] = useState(true);
+
+  const handleCollapse = () => {
+    setSidebarCollapsed(!isSidebarCollapsed);
+  };
+
+  const handleSHForm = () => {
+    setSHForm(!isSHForm);
+  };
+
+  return (
+    <div className="container">
+      <Navbar handleCollapse={handleCollapse} />
+      <div className="content-container">
+        <div className={`sidebar ${isSidebarCollapsed ? '' : 'sidebar-hide'}`}>
+          <Sidebar />
+        </div>
+        <div className={`data-buku ${isSidebarCollapsed ? '' : 'sidebar-hide'}`}>
+          {/* <Form isSHForm={isSHForm} />
+          <div className="sh-form" onClick={handleSHForm}>
+            {isSHForm ? <ChevronsDown size={25}/> : <ChevronsUp size={25}/>}
+          </div>
+          <Data /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
