e7a0e099ba25de8ea71d13446cd0072b4e8b54e7
monamx <129038957+monamx@users.noreply.github.com>
2024-03-02 15:44:39 +0700

    Update page.tsx

--- app/components/Sidebar/page.tsx
+++ app/components/Sidebar/page.tsx
@@ -1,4 +1,4 @@
-import { Settings, Bookmark, BookOpen, Database } from 'react-feather';
+import { Settings, Bookmark, BookOpen, Database, Users, List } from 'react-feather';
 import Link from 'next/link';
 
 const Sidebar = () => {
@@ -9,28 +9,40 @@ const Sidebar = () => {
       </li>
       <hr />
       <li>
-        <Link href="/data">
+        <Link href="/dashboard/buku">
           <Database className='mr-2' size={20} strokeWidth={1.5} />Data Management
         </Link>
       </li>
       <hr />
       <li>
-        <Link href="/peminjaman">
+        <Link href="/dashboard/user">
+          <Users className='mr-2' size={20} strokeWidth={1.5} />Users
+        </Link>
+      </li>
+      <hr />
+      <li>
+        <Link href="/dashboard/peminjaman">
           <BookOpen className='mr-2' size={20} strokeWidth={1.5} />Loan Records
         </Link>
       </li>
       <hr />
       <li>
-        <Link href="/koleksi">
-          <Bookmark className='mr-2' size={20} strokeWidth={1.5} />Book Collection
+        <Link href="/dashboard/list">
+          <List className='mr-2' size={20} strokeWidth={1.5} />Lists
         </Link>
       </li>
       <hr />
       <li>
+        <Link href="/dashboard/koleksi">
+          <Bookmark className='mr-2' size={20} strokeWidth={1.5} />Book Collection
+        </Link>
+      </li>
+      {/* <hr />
+      <li>
         <Link href="/pengaturan">
           <Settings className='mr-2' size={20} strokeWidth={1.5} />Settings
         </Link>
-      </li>
+      </li> */}
     </ul>
   );
 };
